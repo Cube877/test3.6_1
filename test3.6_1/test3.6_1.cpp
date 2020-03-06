93c43a4801071f7c65198c9ba51923a504ae86b9
Cube <877211593@qq.com>
2020-03-06 14:27:21 +0800

    无

--- test3.6_1/test3.6_1.cpp
+++ test3.6_1/test3.6_1.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <opencv.hpp>
+#include <iostream>
 
+using namespace cv;
+using namespace std;
 
 int main()
 {
+	cv:Mat src_color = imread("E:\\Picture\\p1.png");
+	std::vector<cv::Mat> channels;
+	cv::split(src_color, channels);
+	cv::Mat B = channels.at(0);
+	cv::Mat G = channels.at(1);
+	cv::Mat R = channels.at(2);
+	cv::imshow("red", R);
+	cv::imshow("green", G);
+	cv::imshow("blue", B);
+	cv::imshow("original Mat", src_color);
+	waitKey(0);
     return 0;
 }
 
